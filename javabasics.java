0ca6a640cbbfa4b6f1c13f4561d40a7c7a33a7db
baliram072x <baliramrathod0008@gmail.com>
2025-09-03 12:23:32 +0530

    java

---
+++ javabasics.java
@@ -0,0 +1,12 @@
+public class javabasics {
+  public static void main(String[] args) {
+    System.out.print("hello world\n");
+    System.out.print("hello world\n");
+    System.out.print("hello world\n");
+
+  }
+}
+// boiler plate for java 
+// ln --> also gives next line 
+// \n gives next line 
+
