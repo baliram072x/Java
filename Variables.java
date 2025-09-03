6de0b16ba9273e1b425dd6c4b395b01b2d298d3b
baliram072x <baliramrathod0008@gmail.com>
2025-09-03 23:20:06 +0530

    intellij

---
+++ Variables.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class Variables{
+    public static void main(String[]args){
+        int a =10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(2*(a+b));
+    }
+}
+
