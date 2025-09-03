cfe726c22f1edf34c63a4a9f53c51c797c16f123
baliram072x <baliramrathod0008@gmail.com>
2025-09-03 14:36:00 +0530

    intellij

--- First.java
+++ First.java
@@ -2,7 +2,7 @@ public class First {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int c = 30;
+        int c = 40;
         System.out.println(a + b + c);
     }
 }
