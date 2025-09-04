4a8a8ea8d7eed17ed2e387c663820d2bf503cef4
baliram072x <baliramrathod0008@gmail.com>
2025-09-04 15:30:08 +0530

    intellij

---
+++ Java-Question/average.java
@@ -0,0 +1,11 @@
+public class average {
+    public static void main(String[] args){
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        int d = (a+b+c)/3;
+        System.out.println(d);
+    }
+}
+
+// yayyy solve very good work baliram
