01a1126f2ff1721ba2eee4767072486335f3b230
baliram072x <baliramrathod0008@gmail.com>
2025-09-15 21:26:28 +0530

    Loops

---
+++ Loops/whileloop.java
@@ -0,0 +1,18 @@
+public class whileloop {
+    public static void main(String[] args){
+        int counter = 0;
+        while(counter<=100){
+            System.out.println("hello , world ");
+            counter++;
+        }
+    }
+}
+
+
+/*
+while(condition){
+----
+do something
+---
+}
+ */
