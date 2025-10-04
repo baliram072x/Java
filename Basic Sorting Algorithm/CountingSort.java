8f973f1913e0a7c1956f14132c0f34fb6ff6ee48
baliram072x <baliramrathod0008@gmail.com>
2025-10-04 08:26:19 +0530

    Algorithm

--- Basic Sorting Algorithm/CountingSort.java
+++ Basic Sorting Algorithm/CountingSort.java
@@ -1,5 +1,34 @@
 public class CountingSort {
+
+    public static void counting(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i =0 ; i<arr.length; i++){
+            largest = Math.max(largest , arr[i]);
+        }
+        int count[] = new int[largest+1];
+        for(int i = 0; i<arr.length; i++){
+            count[arr[i]]++;
+        }
+        // sorting
+        int j =0;
+        for(int i = 0; i<count.length; i++){
+            while(count[i]>0){
+                arr[j] =i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+
     public static void main(String[] args){
-        int srr[] = {1,4,1,3,2,4,3,7};
+        int arr[] = {1,4,1,3,2,4,3,7};
+        counting(arr);
+        printarr(arr);
     }
 }
