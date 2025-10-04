61432173cf3d341e331fbd1ffa40aa8dca835560
baliram072x <baliramrathod0008@gmail.com>
2025-10-04 22:24:44 +0530

    2d array

---
+++ 2D Array/SprialMatrix.java
@@ -0,0 +1,15 @@
+public class SprialMatrix {
+    public static void printsprial(int matrix[][]){
+        int startrow =0;
+
+
+    }
+
+    public static void main(String[] argsr){
+        int matrix[][] = {{1,2,3,4},
+                         {5,6,7,8},
+                         {9,10,11,12},
+                         {13,14,15,16}};
+    }
+
+}
