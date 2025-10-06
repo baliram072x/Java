e844d5dc3504ca933f49a0682c5fd7b0ba54a169
baliram072x <baliramrathod0008@gmail.com>
2025-10-06 20:15:12 +0530

    sprial matrix

---
+++ 2D Array/DigonalSum.java
@@ -0,0 +1,5 @@
+public class DigonalSum {
+    public static void main(String[] args){
+
+    }
+}
--- 2D Array/SprialMatrix.java
+++ 2D Array/SprialMatrix.java
@@ -8,13 +8,37 @@ public class SprialMatrix {
         while (startrow<=endrow && startcol <= endcol){
              // top
             for(int j = startcol;  j<=endcol; j++ ){
-                System.out.println(matrix[startrow][j]+ " ");
+                System.out.print(matrix[startrow][j]+ " ");
             }
 
             // right
+            for(int i = startrow+1; i<= endrow; i++){
+                System.out.print(matrix[i][endcol]+" ");
+            }
+
+            //bottom
+            for (int j = endcol-1; j>= startcol; j--){
+                if (startrow == endrow){
+                    break;
+                }
+                System.out.print(matrix[endrow][j]+" ");
+            }
+
+            //left
+            for(int i= endrow-1; i>=startrow+1; i--){
+                if (startcol == endcol){
+                    break;
+                }
+                System.out.print(matrix[i][startcol]+" ");
+            }
+
+            startrow++;
+            startcol++;
+            endrow--;
+            endcol--;
 
         }
-
+        System.out.println();
 
     }
 
@@ -23,6 +47,8 @@ public class SprialMatrix {
                          {5,6,7,8},
                          {9,10,11,12},
                          {13,14,15,16}};
+
+        printsprial(matrix);
     }
 
 }
