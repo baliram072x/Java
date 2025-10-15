95363c1d588a6d144c1fac73473ae622231b4356
baliram072x <baliramrathod0008@gmail.com>
2025-10-15 21:15:21 +0530

    Strings

---
+++ Strings/InputOutput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name =  sc.nextLine();
+        System.out.println(name);
+
+        String fullName = "Baliram Rathod";
+        System.out.println(fullName.length());
+
+    }
+}
