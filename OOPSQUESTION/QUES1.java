3654905a5941c285e67a1f2d1884f199f8140eae
baliram072x <baliramrathod0008@gmail.com>
2025-11-15 09:53:53 +0530

    QUES

---
+++ OOPSQUESTION/QUES1.java
@@ -0,0 +1,20 @@
+public class QUES1 {
+        public static void main(String[] args){
+            Stu s = new Stu();
+            s.name = "baliram";
+            System.out.println(s.name);
+        }
+    }
+
+    class Stu{
+        String name;
+        int marks;
+
+    }
+
+//======================================================================================
+
+/*
+assign name to object
+ */
+
---
+++ OOPSQUESTION/README.md
@@ -0,0 +1,10 @@
+Q1. which of the following modifiers are not allowed in ffont of class 
+
+a. private ...in java class are not private 
+b. protected ..in kava class are not protected
+c. public 
+d. default
+
+![img.png](img.png)
+
+Q2 .
