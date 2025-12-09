6e53103428126ecd2c16828fd758e00d8342c7e6
baliram072x <baliramrathod0008@gmail.com>
2025-12-09 19:29:15 +0530

    9/12

--- Recursion/problem3.java
+++ Recursion/problem3.java
@@ -1,8 +1,14 @@
 public class problem3 {
-    public static void factorial(){
-
+    public static int  factorial(int n ){
+        if (n == 0){
+            return 1;
+        }
+      int Fnm1 = factorial(n-1);
+      int fn = n *  factorial(n-1);
+      return fn;
     }
     public static void main(String[] args){
-
+        int n = 5;
+        System.out.println(factorial(n));
     }
 }
