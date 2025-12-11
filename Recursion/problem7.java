cab94db5082a6134bf54dcdac0a3163d41cefedc
baliram072x <baliramrathod0008@gmail.com>
2025-12-11 21:19:43 +0530

    11/12

---
+++ Recursion/problem7.java
@@ -0,0 +1,20 @@
+public class problem7 {
+    public static int  tilingproblem(int n ){
+        if (n == 0 || n == 1){
+            return 1;
+        }
+        // kaam
+        // verticle choise
+        int fnm1 = tilingproblem(n-1);
+
+        // horizontal choise
+
+        int fnm2 = tilingproblem(n-2);
+
+        int totways = fnm1 + fnm2;
+        return totways;
+    }
+    public static void main(String[] args){
+        System.out.println(tilingproblem(3));
+    }
+}
