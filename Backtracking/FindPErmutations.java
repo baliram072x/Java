233a482a3a99d8fc43ce037350aaaba0d9eee631
baliram072x <baliramrathod0008@gmail.com>
2026-01-07 00:46:10 +0530

    7/1/26

---
+++ Backtracking/FindPErmutations.java
@@ -0,0 +1,20 @@
+public class FindPErmutations {
+
+    public static void findpermutation(String str  , String ans ){
+        //base case
+        if (str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        //recursion
+        for (int i =0; i<str.length(); i++){
+            char curr = str.charAt(i);
+            String newstr = str.substring(0,i)+str.substring(i+1);
+            findpermutation(newstr,ans+curr);
+        }
+    }
+    public static  void main(String[] args){
+        String Str = "abc";
+        findpermutation(Str," ");
+    }
+}
