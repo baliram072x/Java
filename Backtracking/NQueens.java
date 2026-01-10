3a455bf63f4aef37f34a9e13388f0214878a4540
baliram072x <baliramrathod0008@gmail.com>
2026-01-10 11:07:06 +0530

    10/1/26

---
+++ Backtracking/NQueens.java
@@ -0,0 +1,67 @@
+public class NQueens {
+    //safe function
+public static Boolean issafe(char board[][] , int row ,  int col){
+    // vertical up
+    for (int i = row-1;  i>=0 ; i--){
+        if (board[i][col] == 'Q'){
+            return false;
+        }
+    }
+
+    // digonal left up
+    for ( int i=row-1 , j=col-1; i>=0 && j>=0; i-- , j-- ){
+        if (board[i][j] == 'Q'){
+            return false;
+        }
+    }
+
+    // digonal right up
+    for (int i = row-1 , j=col+1 ; i>=0 && j< board.length; i-- , j++){
+        if (board[i][j] == 'Q'){
+            return false;
+        }
+    }
+    return true;
+}
+    // print function
+    public static void printboard(char board[][]){
+        System.out.println("**-------------chess board------------**");
+        for (int i = 0; i< board.length; i++ ){
+            for (int j = 0; j< board.length; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+// nQueens posibility function
+    public static void nQueens(char board[][], int row){
+        //base
+        if (row == board.length){
+            printboard(board);
+            return;
+        }
+        // column
+        for(int j =0; j< board.length; j++){
+            if (issafe(board , row , j)){
+                board[row][j] = 'Q';
+                nQueens(board ,row+1); // function call
+                board[row][j] = 'X'; // backtracking step
+            }
+
+        }
+    }
+
+    // main function
+    public static void main(String[] args){
+        int n = 4;
+        char board[][] =  new char[n][n];
+
+        for (int i= 0; i<n; i++){
+            for (int j =0; j<n; j++) {
+                board[i][j] = 'X';
+            }
+        }
+        nQueens(board , 0 );
+    }
+}
